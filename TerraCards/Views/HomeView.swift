51d25ac4e7730b04caff9062b0a2a480c1a4bba5
Josephine Delobel <josephine-coding@users.noreply.github.com>
2020-05-07 16:41:08 +0200

    Création de la branche homeview

---
+++ TerraCards/Views/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  TerraCards
+//
+//  Created by Joséphine Delobel on 07/05/2020.
+//  Copyright © 2020 MacBookGP. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
