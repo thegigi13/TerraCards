4cf17d07db4d7c2bca4fea7690b32632748e4627
patate <patate@patatecarree.fr>
2020-05-11 12:03:21 +0200

    Mise à jour de la cassette (images, couleurs). Changement architecture.

--- TerraCards/Views/CardRectoView.swift
+++ TerraCards/Views/CardRectoView.swift
@@ -10,8 +10,90 @@ import SwiftUI
 
 struct CardRectoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            VStack {
+                Text("")
+            }
+            .frame(width: UIScreen.main.bounds.width * 100/100, height: UIScreen.main.bounds.height * 120/100)
+            .background(Color("tree"))
+            .edgesIgnoringSafeArea(.all)
+
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Image(systemName: "xmark")
+                        .frame(width: 20)
+                        .padding()
+                        .overlay(
+                        Circle()
+                        .stroke(lineWidth: 1)
+                        .padding(6)
+                    )
+                    .padding(.trailing, 20)
+                    .padding(.top, 30)
+                    .opacity(0.3)
+
+                    
+                }
+                Image("chene")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 270)
+                    .padding(.top, 30)
+                Spacer()
+                
+                HStack {
+                    Text("Chêne")
+                        .font(.title)
+                    Circle()
+                    .stroke(lineWidth: 1)
+                    .frame(width: 25, height: 25)
+                    .overlay(
+                        Text("?")
+                    )
+                        .opacity(0.3)
+                }
+                Spacer()
+                HStack(spacing: 20) {
+                    VStack {
+                        Image("foret")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:50)
+                        Text("forêt")
+                            .font(.footnote)
+                    }
+                    VStack {
+                        Image("campagne")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:50)
+                        Text("campagne")
+                            .font(.footnote)
+                    }
+
+                }
+                .opacity(0.8)
+                
+                Spacer().frame(height: 30)
+            }
+            .background(Color("cardBackground"))
+            
+
+            .frame(width: UIScreen.main.bounds.width * 85/100, height: UIScreen.main.bounds.height * 75/100)
+            .cornerRadius(70)
+            .shadow(color: Color.black.opacity(0.4), radius: 8, x: 4, y: 4)
+            
+            .rotation3DEffect(Angle(degrees: Double(-18)) , axis: (x: 0, y: 1, z: 0))
+
+        }
+        
+
+
     }
+    
 }
 
 struct CardRectoView_Previews: PreviewProvider {
