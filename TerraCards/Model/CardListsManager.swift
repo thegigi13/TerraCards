55e36831df59e5365250bcddb0e81fd46c572e20
patate <patate@patatecarree.fr>
2020-05-21 22:15:20 +0200

    Dans le CardListsManager : Fonction de remplissage des cartes au lancement en fonction des UserSettings (cartes gagnées précédemment) Fonction pour gagner des Cartes

--- TerraCards/Model/CardListsManager.swift
+++ TerraCards/Model/CardListsManager.swift
@@ -10,6 +10,32 @@ import Foundation
 import SwiftUI
 
 extension CardsLists {
+    func winCards(cards: [Card]) {
+        for wonCard in cards {
+            if self.wonCards.isEmpty || self.wonCards.first(where: {$0.name == wonCard.name}) == nil {
+                self.wonCards.append(wonCard)
+                UserSettings.userCards.append(wonCard.name)
+            }
+        }
+    }
+}
+extension CardsLists {
+    typealias Completion = (Result<CardsLists,APIError>) -> ()
+    
+    func fillLists(completion: @escaping Completion) {
+        print("youhou")
+
+        fetchAllCards {responseFetch in
+            switch responseFetch {
+            case .success:
+                self.fillWonCardsList()
+                completion(.success(self))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
     func fetchAllCards(completion: @escaping APIProvider.Completion) {
         APIProvider.shared.requestAllRecords(){response in
             switch response {
@@ -28,6 +54,7 @@ extension CardsLists {
             }
         }
     }
+
     
     private func fillAllCardsList(with data: Data, completion: @escaping JSONProvider.Completion) {
         JSONProvider.decodeToCardList(from : data) {response in
@@ -37,12 +64,31 @@ extension CardsLists {
                 for cardRecord in cardList.records {
                     self.allCards.append(cardRecord.fields)
                 }
+                completion(.success(cardList))
             case let .failure(.decodingError(error: error)) :
                 completion(.failure(.decodingError(error: error)))
             default :
                 completion(.failure(.unknown))
-                print("on voulait une cardlist et on a autre chose")
+                print("on voulait une cardList et on a autre chose")
             }
         }
+    }
+    
+    private func fillWonCardsList() {
+        var result: [Card] = []
+        
+        for cardName in UserSettings.userCards {
+            let wonCard = allCards.first(where: {
+                    $0.name == cardName
+            })
+            if let wonCard = wonCard {
+                result.append(wonCard)
+            }
+        }
+        print("Voici les cartes déjà obtenues par le joueur :")
+        for re in result {
+            print(re.name ?? "")
+        }
+        self.wonCards = result
     }
 }
